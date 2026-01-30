f4c650899228947d6e1fcbaf907fbcea7ab53c54
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-01-30 18:00:25 +0000

    started

---
+++ horse_race.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void advance(int horseNum, int* horse_position)
+void printLane(int horseNum, int* horse_position)
+bool isWinner(int horseNum, int* horse_position)
+
+int main() {
+	int horse_position[0,0,0,0,0];
+	for():
+		printLane();	
+} //end main
+
+int printLane(int horseNum, int* horse_position) {
+	
+} //end printLane
