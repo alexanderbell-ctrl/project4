add91209f90dc16780d5034b326651f7277e15ac
alexanderbell-ctrl <alexander.bell@bsu.edu>
2026-01-30 20:22:14 +0000

    'advance' coin flip working correctly

--- horse_race.cpp
+++ horse_race.cpp
@@ -7,11 +7,31 @@ void printLane(int, int*);
 bool isWinner(int, int*);
 
 int main() {
+	std::srand(std::time(NULL));
+
 	int track_positions[5] ={0,0,0,0,0};
 	for(int i = 0; i < 5; ++i) {
 		printLane(i, track_positions);
 	} //end for loop
+	
+	bool keepGoing = true;
+	while(keepGoing) {
+		for(int horse = 0; horse < 5; ++horse) {
+			advance(horse, track_positions);
+		} //end for loop
+		keepGoing = false;
+	} //end while loop
 } //end main
+
+void advance(int horseNum, int* horse_position) {	
+	int coin_flip = (std::rand() % 2) +1;
+	std::cout << "coin flip: " << coin_flip << "\n";
+	if(coin_flip == 2) {
+		std::cout << "horse #" << horseNum << "advances 1\n";
+	} //end if
+} //end advance
+
+
 
 void printLane(int horseNum, int* horse_position) {
 	for(int i = 0; i < horse_position[horseNum]; ++i) {
